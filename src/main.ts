784cdf8971f779a9f0a5a86d0f59a07110ab1828
zzoo <zzooxox@gmail.com>
2024-05-07 23:43:14 +0900

    refactor: Update main.ts to use Mustit Blog title and description for Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Mustit Blog')
+    .setDescription('sexy documentation for Mustit Blog API')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
