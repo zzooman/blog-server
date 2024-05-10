59a746145c01dae59dd581a82c52fbd07f5b9d16
주영석 <ysjoo@mustit.co.kr>
2024-05-10 13:56:33 +0900

    chore: Update npm dependencies for cookie-parser and @types/cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const config = new DocumentBuilder()
     .setTitle('Mustit Blog')
     .setDescription('sexy documentation for Mustit Blog API')
@@ -13,6 +13,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: true,
+    credentials: true, // 크로스 도메인 요청에도 쿠키 전송
+  });
 
   await app.listen(9900);
 }
