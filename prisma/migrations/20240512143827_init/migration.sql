99eab681c922824dbd82d267b7da74cd0e1e48ae
주영석 <ysjoo@mustit.co.kr>
2024-05-12 23:38:53 +0900

    refactor: Update database schema and migrations
    This commit updates the database schema and migrations to include the new `Comment` and `Likes` models. It also drops the `likes` column from the `Post` table. These changes ensure that the database reflects the new relationships between users, posts, comments, and likes. It improves the overall data structure and allows for more advanced querying and data manipulation.

--- prisma/migrations/20240512143827_init/migration.sql
+++ prisma/migrations/20240512143827_init/migration.sql
@@ -1,11 +1,31 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "Division" AS ENUM ('frontend', 'backend');
 
-  - You are about to drop the column `likes` on the `Post` table. All the data in the column will be lost.
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "division" "Division" NOT NULL,
 
-*/
--- AlterTable
-ALTER TABLE "Post" DROP COLUMN "likes";
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "lowContent" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT true,
+    "thumbnail" TEXT,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Comment" (
@@ -29,6 +49,12 @@ CREATE TABLE "Likes" (
 
     CONSTRAINT "Likes_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
