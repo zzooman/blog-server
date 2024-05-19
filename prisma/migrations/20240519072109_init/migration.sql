79e7a70c20749e0e77cb6a0f381e9ae7128a51da
주영석 <ysjoo@mustit.co.kr>
2024-05-19 16:21:16 +0900

    refactor: Update database schema and migrations
    This commit updates the database schema and migrations to include the new `Comment` and `Likes` models. It also drops the `likes` column from the `Post` table. These changes ensure that the database reflects the new relationships between users, posts, comments, and likes. It improves the overall data structure and allows for more advanced querying and data manipulation.

--- prisma/migrations/20240519072109_init/migration.sql
+++ prisma/migrations/20240519072109_init/migration.sql
@@ -5,6 +5,7 @@ CREATE TYPE "Division" AS ENUM ('frontend', 'backend');
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "division" "Division" NOT NULL,
 
@@ -52,6 +53,9 @@ CREATE TABLE "Likes" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Article" ADD CONSTRAINT "Article_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
